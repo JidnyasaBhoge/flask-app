0ae76f37a750a8361e2f099a343c253b91bd0c37
JidnyasaBhoge <87643687+JidnyasaBhoge@users.noreply.github.com>
2025-01-07 13:17:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from flask import Flask, jsonify
-import os
-import MySQLdb
+from flask import Flask
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = MySQLdb.connect(
-        host=os.getenv('MYSQL_HOST', 'localhost'),
-        user=os.getenv('MYSQL_USER', 'root'),
-        password=os.getenv('MYSQL_PASSWORD', 'root'),
-        database=os.getenv('MYSQL_DB', 'devops')
-    )
-    return conn
-
 @app.route('/')
 def home():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users')
-    users = cursor.fetchall()
-    conn.close()
-    return jsonify(users)
+    return "Hello, Flask!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
